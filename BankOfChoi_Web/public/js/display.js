a2de17546b3b0a9fb230535211d0297e73cf106a
ghalander <hector.med27@gmail.com>
2018-07-29 23:04:29 -0700

    styled, demo, and cleaned

--- BankOfChoi_Web/public/js/display.js
+++ BankOfChoi_Web/public/js/display.js
@@ -1,3 +1,10 @@
+// Waits for everything to load before we start populating fields
+// test account object exists in page since 'account.js' loaded first
+//TODO: Dynamic Select once it's linked up?
+// $( document ).ready(function() {
+//
+// });
+
 // In the start of the display tabs, theres an 'onclick' attribute with the function
 // changeMessage(event)
 // 'event' is an inherit property for the html page
@@ -11,4 +18,16 @@ function changeMessage(e){
   //console.log(e);
   //console.log(e.target);
   $("#messageBox").text(e.target.text);
+}
+
+function changeHistory(){
+  for(var i = Object.keys(account.transactionNum).length-1; i > -1; i--){
+    $("#historyList").append(
+      "<div class='row'>" +
+      "<div class='col-md-4'>" + account.transactionNum[i] + "</div>" +
+      "<div class='col-md-4'>" + account.transactionName[i] + "</div>" +
+      "<div class='col-md-4'>" + account.transactionAmount[i] + "</div>" +
+      "</div><hr/>"
+    );
+  }
 }
